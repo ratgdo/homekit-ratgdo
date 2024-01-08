b22da31a38eefa0c6ef6722e3eb400ef13ff692c
David Kerr <david@kerrfamily.org>
2024-01-08 14:52:29 -0500

    Web page updates
    Add authentication, allow OTA updates from github, add door controls.
    Default login admin/password
    Make images const to serve from flash and reduce memory usage

--- src/web.cpp
+++ src/web.cpp
@@ -2,16 +2,24 @@
 // Copyright (c) 2023-24 David Kerr, https://github.com/dkerr64
 // All rights reserved. GPLv3 License
 
+// Browser cache control, time in seconds after which browser cache invalid
+// This is used for CSS, JS and IMAGE file types.
+#define CACHE_CONTROL 3600
+
+#include <string>
+#include <tuple>
+#include <unordered_map>
+
 #include "www/build/webcontent.h"
+
+#include "ratgdo.h"
+#include "comms.h"
 
 #include <arduino_homekit_server.h>
 #include <ESP8266WebServer.h>
 #include <ESP8266HTTPUpdateServer.h>
+#include <LittleFS.h>
 #include "log.h"
-#include "ratgdo.h"
-
-#include <ESP8266WebServer.h>
-#include <ESP8266HTTPUpdateServer.h>
 #include "utilities.h"
 
 ESP8266WebServer server(80);
@@ -19,101 +27,181 @@ ESP8266HTTPUpdateServer httpUpdater(true);
 
 void handle_reset();
 void handle_reboot();
-void handle_notfound();
-void handle_handlestatus();
-
-extern struct GarageDoor garage_door;
+void handle_status();
+void handle_settings();
+void handle_everything();
+void handle_setgdo();
+void handle_logout();
 
 // Make device_name available
 extern "C" char device_name[];
+// Garage door status
+extern struct GarageDoor garage_door;
 
-/********* main loop **********/
+// userid/password
+const char www_username[] = "admin";
+const char www_password[] = "password";
+const char www_realm[] = "RATGDO Login Required";
 
-void setup_web()
-{
-    server.on("/status.json", HTTP_GET, handle_handlestatus);
-    server.on("/reset", HTTP_POST, handle_reset);
-    server.on("/reboot", HTTP_POST, handle_reboot);
-
-    server.onNotFound(handle_notfound);
-
-    httpUpdater.setup(&server);
-
-    server.begin();
-    RINFO("HTTP server started");
-}
+// MD5 Hash of "user:realm:password"
+char www_credentials[48] = "10d3c00fa1e09696601ef113b99f8a87";
+const char credentials_file[] = "www_credentials";
 
 void web_loop()
 {
     server.handleClient();
 }
 
-/********* handlers **********/
+const std::unordered_multimap<std::string, std::pair<const HTTPMethod, void (*)()>> builtInUri = {
+    {"/status.json", {HTTP_GET, handle_status}},
+    {"/reset", {HTTP_POST, handle_reset}},
+    {"/reboot", {HTTP_POST, handle_reboot}},
+    {"/setgdo", {HTTP_POST, handle_setgdo}},
+    {"/logout", {HTTP_GET, handle_logout}},
+    {"/settings.html", {HTTP_GET, handle_settings}},
+    {"/", {HTTP_GET, handle_everything}}};
 
+void setup_web()
+{
+    RINFO("Starting server");
+
+    // www_credentials = server.credentialHash(www_username, www_realm, www_password);
+    File file = LittleFS.open(credentials_file, "r");
+    if (!file)
+    {
+        RINFO("www_credentials file doesn't exist. creating...");
+        file = LittleFS.open(credentials_file, "w");
+        file.print(www_credentials);
+    }
+    else
+    {
+        RINFO("Reading www_credentials from file");
+        strncpy(www_credentials, file.readString().c_str(), 48);
+    }
+    file.close();
+    RINFO("WWW Credentials: %s", www_credentials);
+
+    RINFO("Registering URI handlers");
+    // Register URI handlers for URIs that have built-in handlers in this source file.
+    for (auto uri : builtInUri)
+    {
+        HTTPMethod method = std::get<1>(uri).first;
+        void (*handler)() = std::get<1>(uri).second;
+        RINFO("Register: %s", uri.first.c_str());
+        server.on(uri.first.c_str(), method, handler);
+    }
+    // Register URI handlers for URIs that are "external" files
+    for (auto uri : webcontent)
+    {
+        // Only register those that are not duplicates of built-in handlers.
+        if (builtInUri.find(uri.first) == builtInUri.end())
+        {
+            RINFO("Register: %s", uri.first.c_str());
+            server.on(uri.first.c_str(), HTTP_GET, handle_everything);
+        }
+    }
+    // xSemaphoreHttpd = xSemaphoreCreateMutex();
+    server.onNotFound(handle_everything);
+    httpUpdater.setup(&server);
+    server.begin();
+    RINFO("HTTP server started");
+    return;
+}
+
+/********* handlers **********/
 void handle_reset()
 {
+    if (!server.authenticateDigest(www_username, www_credentials))
+    {
+        return server.requestAuthentication(DIGEST_AUTH, www_realm);
+    }
     RINFO("... reset requested");
+    const char *resp = "<p>This device has been un-paired from HomeKit.</p><p><a href=\"/\">Back</a></p>";
     homekit_storage_reset();
-    server.send(
-        200,
-        "text/html",
-        "<p>This device has been un-paired from HomeKit.</p>"
-        "<p><a href=\"/\">Back</a></p>");
+    server.send(200, resp);
+    return;
 }
 
 void handle_reboot()
 {
     RINFO("... reboot requested");
-    server.send(
-        200,
-        "text/html",
+    const char *resp =
         "<head>"
         "<meta http-equiv=\"refresh\" content=\"15;url=/\" />"
         "</head>"
         "<body>"
         "<p>RATGDO restarting. Please wait. Reconnecting in 15 seconds...</p>"
         "<p><a href=\"/\">Back</a></p>"
-        "</body>");
-
+        "</body>";
+    server.send(200, resp);
     server.stop();
     sync_and_restart();
+    return;
 }
 
-void handle_notfound()
+void load_page(const char *page)
 {
-    String page = server.uri();
-
-    if (page == "/")
+    if (webcontent.count(page) > 0)
     {
-        page = "/index.html";
-    }
+        const unsigned char *data = std::get<0>(webcontent.at(page));
+        const unsigned int length = std::get<1>(webcontent.at(page));
+        const char *type = std::get<2>(webcontent.at(page));
+        // Following for browser cache control...
+        const char *md5 = std::get<3>(webcontent.at(page)).c_str();
+        bool cache = false;
+        char cacheHdr[24] = "no-cache, no-store";
+        char matchHdr[48] = "";
 
-    if (webcontent.count(page.c_str()) > 0)
-    {
-        unsigned char *data;
-        unsigned int length;
-        char *type;
-        std::tie(data, length, type) = webcontent[page.c_str()];
-        RINFO("Sending gzip data for: %s (type %s, length %i)", page.c_str(), type, length);
-        server.sendHeader("Content-Encoding", "gzip");
-        server.send_P(200, type, (const char *)data, length);
+        if ((CACHE_CONTROL > 0) &&
+            (!strcmp(type, "text/css") || !strcmp(type, "text/javascript") || strstr(type, "image")))
+        {
+            sprintf(cacheHdr, "max-age=%i", CACHE_CONTROL);
+            cache = true;
+        }
+
+        server.sendHeader("Cache-Control", cacheHdr);
+        if (cache)
+            server.sendHeader("ETag", md5);
+
+        if (server.hasHeader("If-None-Match"))
+            strncpy(matchHdr, server.header("If-None-Match").c_str(), 48);
+
+        if (strcmp(md5, matchHdr))
+        {
+            RINFO("Sending gzip data for: %s (type %s, length %i)", page, type, length);
+            server.sendHeader("Content-Encoding", "gzip");
+            server.send_P(200, type, (const char *)data, length);
+        }
+        else
+        {
+            RINFO("Sending 304 Not Modified for: %s (type %s)", page, type);
+            server.send(304, type, "", 0);
+        }
     }
     else
     {
-        RINFO("Sending 404 not found for: %s", page.c_str());
+        RINFO("Sending 404 not found for: %s", page);
         server.send(404, "text/plain", "404: Not Found");
     }
     return;
 }
 
-void handle_handlestatus()
+void handle_everything()
 {
-    homekit_server_t *hks = arduino_homekit_get_running_server();
+    String page = server.uri();
+    if (page == "/")
+        load_page("/index.html");
+    else
+        load_page(page.c_str());
+}
 
-    // If arguments passed with the URL then only a subset of the
-    // status fields are requested...
-    std::unordered_map<std::string, bool> argReq;
+void handle_status()
+{
     bool all = true;
+    char json[512] = ""; // Maximum length of JSON response
+
+    // find query string and macro to test if arg is present
+    std::unordered_map<std::string, bool> argReq;
     if (server.args() > 0)
     {
         all = false;
@@ -123,90 +211,200 @@ void handle_handlestatus()
         }
     }
 
+#define HAS_ARG(arg) argReq[arg]
+#define upTime millis()
+#define paired homekit_is_paired()
+#define accessoryID arduino_homekit_get_running_server()->accessory_id
+#define localIP WiFi.localIP().toString().c_str()
+#define subnetMask WiFi.subnetMask().toString().c_str()
+#define gatewayIP WiFi.gatewayIP().toString().c_str()
+#define macAddress WiFi.macAddress().c_str()
+#define wifiSSID WiFi.SSID().c_str()
+// Helper macros to add int, string or boolean to a json format string.
+#define ADD_INT(s, k, v)                      \
+    {                                         \
+        strcat(s, "\"");                      \
+        strcat(s, (k));                       \
+        strcat(s, "\": ");                    \
+        strcat(s, std::to_string(v).c_str()); \
+        strcat(s, ",\n");                     \
+    }
+#define ADD_STR(s, k, v)     \
+    {                        \
+        strcat(s, "\"");     \
+        strcat(s, (k));      \
+        strcat(s, "\": \""); \
+        strcat(s, (v));      \
+        strcat(s, "\",\n");  \
+    }
+#define ADD_BOOL(s, k, v)                  \
+    {                                      \
+        strcat(s, "\"");                   \
+        strcat(s, (k));                    \
+        strcat(s, "\": ");                 \
+        strcat(s, (v) ? "true" : "false"); \
+        strcat(s, ",\n");                  \
+    }
+
     // Build the JSON string
-    // Note that newlines and indentation are not required within the json,
-    // but are included to improve readability in the console log.
-    std::string json = "{\n"; // open the json
-    if (all || argReq["uptime"])
-        json.append(std::string("  \"upTime\": ") + std::to_string(millis()) + ",\n");
+    strcat(json, "{\n");
+    if (all || HAS_ARG("uptime"))
+        ADD_INT(json, "upTime", upTime);
     if (all)
-        json.append(std::string("  \"deviceName\": \"") + device_name + "\",\n");
+        ADD_STR(json, "deviceName", device_name);
     if (all)
-        json.append(std::string("  \"paired\": ") + (homekit_is_paired() ? "true" : "false") + ",\n");
+        ADD_BOOL(json, "paired", paired);
     if (all)
-        json.append(std::string("  \"firmwareVersion\": \"") + std::string(AUTO_VERSION) + "\",\n");
+        ADD_STR(json, "firmwareVersion", std::string(AUTO_VERSION).c_str());
     if (all)
-        json.append(std::string("  \"accessoryID\": \"") + hks->accessory_id + "\",\n");
+        ADD_STR(json, "accessoryID", accessoryID);
     if (all)
-        json.append(std::string("  \"localIP\": \"") + WiFi.localIP().toString().c_str() + "\",\n");
+        ADD_STR(json, "localIP", localIP);
     if (all)
-        json.append(std::string("  \"subnetMask\": \"") + WiFi.subnetMask().toString().c_str() + "\",\n");
+        ADD_STR(json, "subnetMask", subnetMask);
     if (all)
-        json.append(std::string("  \"gatewayIP\": \"") + WiFi.gatewayIP().toString().c_str() + "\",\n");
+        ADD_STR(json, "gatewayIP", gatewayIP);
     if (all)
-        json.append(std::string("  \"macAddress\": \"") + WiFi.macAddress().c_str() + "\",\n");
+        ADD_STR(json, "macAddress", macAddress);
     if (all)
-        json.append(std::string("  \"wifiSSID\": \"") + WiFi.SSID().c_str() + "\",\n");
-    if (all || argReq["doorstate"])
+        ADD_STR(json, "wifiSSID", wifiSSID);
+    if (all || HAS_ARG("doorstate"))
     {
-        std::string doorState = "";
         switch (garage_door.current_state)
         {
         case 0:
-            doorState = "Open";
+            ADD_STR(json, "garageDoorState", "Open");
             break;
         case 1:
-            doorState = "Closed";
+            ADD_STR(json, "garageDoorState", "Closed");
             break;
         case 2:
-            doorState = "Opening";
+            ADD_STR(json, "garageDoorState", "Opening");
             break;
         case 3:
-            doorState = "Closing";
+            ADD_STR(json, "garageDoorState", "Closing");
             break;
         case 4:
-            doorState = "Stopped";
+            ADD_STR(json, "garageDoorState", "Stopped");
             break;
         default:
-            doorState = "Unknown";
+            ADD_STR(json, "garageDoorState", "Unknown");
         }
-        json.append(std::string("  \"garageDoorState\": \"") + doorState + "\",\n");
     }
-    if (all || argReq["lockstate"])
+    if (all || HAS_ARG("lockstate"))
     {
-        std::string lockState = "";
         switch (garage_door.current_lock)
         {
         case 0:
-            lockState = "Unsecured";
+            ADD_STR(json, "garageLockState", "Unsecured");
             break;
         case 1:
-            lockState = "Secured";
+            ADD_STR(json, "garageLockState", "Secured");
             break;
         case 2:
-            lockState = "Jammed";
+            ADD_STR(json, "garageLockState", "Jammed");
             break;
         default:
-            lockState = "Unknown";
+            ADD_STR(json, "garageLockState", "Unknown");
         }
-        json.append(std::string("  \"garageLockState\": \"") + lockState + "\",\n");
     }
-    if (all || argReq["lighton"])
-        json.append(std::string("  \"garageLightOn\": ") + (garage_door.light ? "true" : "false") + ",\n");
-    if (all || argReq["motion"])
-        json.append(std::string("  \"garageMotion\": ") + (garage_door.motion ? "true" : "false") + ",\n");
-    if (all || argReq["obstruction"])
-        json.append(std::string("  \"garageObstructed\": ") + (garage_door.obstructed ? "true" : "false") + ",\n");
+    if (all || HAS_ARG("lighton"))
+        ADD_BOOL(json, "garageLightOn", garage_door.light)
+    if (all || HAS_ARG("motion"))
+        ADD_BOOL(json, "garageMotion", garage_door.motion)
+    if (all || HAS_ARG("obstruction"))
+        ADD_BOOL(json, "garageObstructed", garage_door.obstructed)
 
     // remove the final comma/newline to ensure valid JSON syntax
-    json.erase(json.rfind(",\n"));
-    json.append("\n}"); // close the json
-
+    json[strlen(json) - 2] = 0;
+    // Terminate json with close curly
+    strcat(json, "\n}");
     // Only log if all requested (no arguments).
     // Avoids spaming console log if repeated requests for one value.
     if (all)
-        RINFO("Status requested:\n%s", json.c_str());
+        RINFO("Status requested:\n%s", json);
+    server.sendHeader("Cache-Control", "no-cache, no-store");
+    server.send(200, "application/json", json);
+    return;
+}
 
-    server.send(200, "application/json", json.c_str());
+void handle_settings()
+{
+    if (!server.authenticateDigest(www_username, www_credentials))
+    {
+        RINFO("In settings request authentication");
+        return server.requestAuthentication(DIGEST_AUTH, www_realm);
+    }
+    return load_page("/settings.html");
+}
+
+void handle_logout()
+{
+    RINFO("Handle logout");
+    /*
+    int nHeaders = server.headers();
+    for (int i = 0; i < nHeaders; i++) {
+        RINFO("%s : %s", server.headerName(i).c_str(), server.header(i).c_str());
+    }
+    */
+    return server.requestAuthentication(DIGEST_AUTH, www_realm);
+}
+
+void handle_setgdo()
+{
+    char key[20] = "";
+    char value[48] = "";
+    RINFO("In setGDO");
+    if (!server.authenticateDigest(www_username, www_credentials))
+    {
+        RINFO("In setGDO request authentication");
+        return server.requestAuthentication(DIGEST_AUTH, www_realm);
+    }
+
+    strncpy(key, server.argName(0).c_str(), 20);
+    strncpy(value, server.arg(0).c_str(), 48);
+    RINFO("Key: %s, Value: %s", key, value);
+    if (strlen(key) == 0 || strlen(value) == 0)
+    {
+        RINFO("Sending 400 bad request, missing argument, for: %s", server.uri().c_str());
+        server.send(400, "text/plain", "400: Bad Request, missing argument");
+        return;
+    }
+    if (!server.authenticateDigest(www_username, www_credentials))
+    {
+        RINFO("In setGDO request authentication");
+        return server.requestAuthentication(DIGEST_AUTH, www_realm);
+    }
+
+    if (!strcmp(key, "lighton"))
+    {
+        set_light(!strcmp(value, "1") ? true : false);
+    }
+    else if (!strcmp(key, "doorstate"))
+    {
+        if (!strcmp(value, "1"))
+            open_door();
+        else
+            close_door();
+    }
+    else if (!strcmp(key, "lockstate"))
+    {
+        set_lock(!strcmp(value, "1") ? 1 : 0);
+    }
+    else if (!strcmp(key, "credentials"))
+    {
+        strncpy(www_credentials, server.arg(0).c_str(), 48);
+        RINFO("Writing new www_credentials to file: %s", www_credentials);
+        File file = LittleFS.open(credentials_file, "w");
+        file.print(www_credentials);
+        file.close();
+    }
+    else
+    {
+        RINFO("Sending 400 bad request, missing argument, for: %s", server.uri().c_str());
+        server.send(400, "text/plain", "400: Bad Request, missing argument");
+    }
+    RINFO("SetGDO Complete");
+    server.send(200, "text/html", "<p>Success.</p>");
     return;
 }
