310e3a0af0a71599d4994d705d83b98894af151e
David Kerr <david@kerrfamily.org>
2024-04-06 16:36:59 -0400

    Add a memory buffer for log messages that is saved to file if system crashes

--- lib/ratgdo/log.h
+++ lib/ratgdo/log.h
@@ -4,24 +4,44 @@
 #ifndef _LOG_H
 #define _LOG_H
 
+#include <Arduino.h>
 #include "secplus2.h"
+#include <esp_xpgm.h>
 
 void print_packet(uint8_t pkt[SECPLUS2_CODE_LEN]);
 
-#ifndef UNIT_TEST
+// #define LOG_MSG_BUFFER
 
-#include <esp_xpgm.h>
+#ifdef LOG_MSG_BUFFER
+
+#define LOG_MSG_FILE "crash_log"
+typedef struct logBuffer
+{
+    uint32_t head;
+    char buffer[2044];
+} logBuffer;
+
+void logToBuffer_P(const char *fmt, ...);
+void printLogBuffer(Print &outDevice = Serial);
+void crashCallback();
+
+#define RINFO(message, ...) logToBuffer_P(PSTR(">>> [%7d] RATGDO: " message "\r\n"), millis(), ##__VA_ARGS__)
+#define RERROR(message, ...) logToBuffer_P(PSTR("!!! [%7d] RATGDO: " message "\r\n"), millis(), ##__VA_ARGS__)
+#else // LOG_MSG_BUFFER
+
+#ifndef UNIT_TEST
 
 #define RINFO(message, ...) XPGM_PRINTF(">>> [%7d] RATGDO: " message "\r\n", millis(), ##__VA_ARGS__)
 #define RERROR(message, ...) XPGM_PRINTF("!!! [%7d] RATGDO: " message "\r\n", millis(), ##__VA_ARGS__)
 
-#else  // UNIT_TEST
+#else // UNIT_TEST
 
 #include <stdio.h>
-
 #define RINFO(message, ...) printf(">>> RATGDO: " message "\n", ##__VA_ARGS__)
 #define RERROR(message, ...) printf("!!! RATGDO: " message "\n", ##__VA_ARGS__)
 
 #endif // UNIT_TEST
+
+#endif // LOG_MSG_BUFFER
 
 #endif // _LOG_H
--- src/log.cpp
+++ src/log.cpp
@@ -4,16 +4,20 @@
 #include <stdint.h>
 
 #include "log.h"
+#include "utilities.h"
 #include "secplus2.h"
+#include <umm_malloc/umm_malloc.h>
+#include <umm_malloc/umm_heap_select.h>
 
 #ifndef UNIT_TEST
 
 #include <Arduino.h>
 
-void print_packet(uint8_t pkt[SECPLUS2_CODE_LEN]) {
+void print_packet(uint8_t pkt[SECPLUS2_CODE_LEN])
+{
     RINFO("decoded packet: [%02X%02X%02X%02X%02X%02X%02X%02X%02X%02X%02X%02X%02X%02X%02X%02X%02X%02X%02X]",
-            pkt[0], pkt[1], pkt[2], pkt[3], pkt[4], pkt[5], pkt[6], pkt[7], pkt[8], pkt[9],
-            pkt[10], pkt[11], pkt[12], pkt[13], pkt[14], pkt[15], pkt[16], pkt[17], pkt[18]);
+          pkt[0], pkt[1], pkt[2], pkt[3], pkt[4], pkt[5], pkt[6], pkt[7], pkt[8], pkt[9],
+          pkt[10], pkt[11], pkt[12], pkt[13], pkt[14], pkt[15], pkt[16], pkt[17], pkt[18]);
 }
 
 #else // UNIT_TEST
@@ -21,3 +25,71 @@ void print_packet(uint8_t pkt[SECPLUS2_CODE_LEN]) {
 void print_packet(uint8_t pkt[SECPLUS2_CODE_LEN]) {}
 
 #endif // UNIT_TEST
+
+#ifdef LOG_MSG_BUFFER
+char *lineBuffer = NULL;
+logBuffer *msgBuffer = NULL;
+
+void logToBuffer_P(const char *fmt, ...)
+{
+    if (!lineBuffer)
+    {
+        // first time in we need to create the buffers
+        HeapSelectIram ephemeral;
+        lineBuffer = (char *)malloc(1024);
+        msgBuffer = (logBuffer *)malloc(sizeof(logBuffer));
+        msgBuffer->head = 0;
+    }
+    // parse the format string into lineBuffer
+    va_list args;
+    va_start(args, fmt);
+    vsnprintf_P(lineBuffer, 1024, fmt, args);
+    va_end(args);
+    // print line to the serial port
+    Serial.print(lineBuffer);
+    // copy the line into the message save buffer
+    size_t len = strlen(lineBuffer);
+    size_t available = sizeof(msgBuffer->buffer) - msgBuffer->head;
+    memcpy(&msgBuffer->buffer[msgBuffer->head], lineBuffer, min(available, len));
+    if (available < len)
+    {
+        // we wrapped on the available buffer space
+        msgBuffer->head = len - available;
+        memcpy(msgBuffer->buffer, &lineBuffer[available], msgBuffer->head);
+    }
+    else
+    {
+        msgBuffer->head += len;
+    }
+}
+
+void crashCallback()
+{
+    if (!msgBuffer)
+        return;
+    write_data_to_file(LOG_MSG_FILE, msgBuffer, sizeof(logBuffer));
+}
+
+void printLogBuffer(Print &outputDev)
+{
+    if (!msgBuffer)
+        return;
+    logBuffer *savedLogs = NULL;
+    {
+        HeapSelectIram ephemeral;
+        savedLogs = (logBuffer *)malloc(sizeof(logBuffer));
+    }
+    if (!savedLogs)
+        return;
+
+    if (read_data_from_file(LOG_MSG_FILE, savedLogs, sizeof(logBuffer)))
+    {
+        outputDev.println();
+        outputDev.write(&savedLogs->buffer[savedLogs->head], sizeof(savedLogs->buffer) - savedLogs->head);
+        outputDev.write(savedLogs->buffer, savedLogs->head);
+        outputDev.println();
+    }
+    free(savedLogs);
+}
+
+#endif
