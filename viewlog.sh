a5e1b763c13f250a64773a6254757de928309f80
David Kerr <david@kerrfamily.org>
2024-05-09 22:12:35 -0400

    Add command line firmware upload script

--- viewlog.sh
+++ viewlog.sh
@@ -1,4 +1,6 @@
+#!/usr/bin/env sh
 UUID=$(uuidgen)
 URL=$(curl -s "http://${1}/rest/events/subscribe?id=${UUID}&log")
 curl -s "http://${1}/showlog"
 curl -s -N "http://${1}${URL}?id=${UUID}" | sed -u -n '/event: logger/{n;p;}' | cut -c 7-
+exit 0
