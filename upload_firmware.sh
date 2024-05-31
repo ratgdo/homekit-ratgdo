4f68af380c6ca8025b920f08f90d42ade6678889
David Kerr <david@kerrfamily.org>
2024-05-30 20:00:35 -0400

    Add error checking to upload_firmware script

--- upload_firmware.sh
+++ upload_firmware.sh
@@ -5,7 +5,7 @@ if [ $(which md5sum) ]; then
     # Linux
     MD5=$(md5sum "${FILE}" | cut -d' ' -f1)
     SIZE=$(stat -c%s "${FILE}")
-elif [ $(which md5) ]; then
+    elif [ $(which md5) ]; then
     # macOS
     MD5=$(md5 -q "${FILE}")
     SIZE=$(stat -f%z "${FILE}")
@@ -19,9 +19,22 @@ JSON="{\"md5\":\"${MD5}\",\"size\":${SIZE},\"uuid\":\"n/a\"}"
 #echo "Inform host of file size and MD5 hash..."
 #curl -s -X POST -F "updateUnderway=${JSON}" "http://${IP}/setgdo" > /dev/null
 echo "Uploading file..."
-curl -s -F "content=@${FILE}" "http://${IP}/update?action=update&size=${SIZE}&md5=${MD5}"
-echo "Upload complete, verifying... "
-curl -s -F "content=@${FILE}" "http://${IP}/update?action=verify&size=${SIZE}&md5=${MD5}"
-echo "Reboot RATGDO..."
-curl -s -X POST "http://${IP}/reboot"
-exit 0
+RESPONSE=$(curl -s -w ">>>>>%{http_code}" -F "content=@${FILE}" "http://${IP}/update?action=update&size=${SIZE}&md5=${MD5}")
+BODY=$(echo ${RESPONSE} | awk -F'>>>>>' '{print $1}')
+RC=$(echo ${RESPONSE} | awk -F'>>>>>' '{print $2}')
+echo ${BODY}
+if [ "${RC}" = "200" ]; then
+    echo "Upload complete, verifying... "
+    RESPONSE=$(curl -s -w ">>>>>%{http_code}" -F "content=@${FILE}" "http://${IP}/update?action=verify&size=${SIZE}&md5=${MD5}")
+    BODY=$(echo ${RESPONSE} | awk -F'>>>>>' '{print $1}')
+    RC=$(echo ${RESPONSE} | awk -F'>>>>>' '{print $2}')
+    echo ${BODY}
+    if [ "${RC}" = "200" ]; then
+        echo "Success, reboot RATGDO, please allow 30 seconds to complete..."
+        curl -s -X POST "http://${IP}/reboot"
+        exit 0
+    fi
+fi
+echo "Upload failure, RATGDO device NOT rebooted..."
+echo "To reboot device issue command: curl -s -X POST http://${IP}/reboot"
+exit 1
