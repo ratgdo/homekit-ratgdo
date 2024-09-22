6946c70b9d39f873e59d0f728ea18fc7ec79572d
Donavan Becker <beckersmarthome@icloud.com>
2024-09-22 18:20:44 -0500

    Add Pre-Release Worklfow that Triggers on Push

--- auto_firmware_version.py
+++ auto_firmware_version.py
@@ -3,9 +3,21 @@ import json
 
 Import("env")
 
+
+# Define the paths
+primary_path = '/home/runner/work/homekit-ratgdo/homekit-ratgdo/docs/manifest.json'
+fallback_path = './docs/manifest.json'
+
 def get_firmware_specifier_build_flag():
-    f = open('./docs/manifest.json')
-    data = json.load(f)
+    # Try to open the primary path, if it fails, use the fallback path
+    try:
+        with open(primary_path) as f:
+            data = json.load(f)
+            print ("Primary path used")
+    except FileNotFoundError:
+        with open(fallback_path, 'r') as f:
+            data = json.load(f)
+            print ("Fallback path used")
     f.close()
     build_version = data['version'].replace('v', '', 1) #remove letter v from front of version string
     build_flag = "-D AUTO_VERSION=\\\"" + build_version + "\\\""
