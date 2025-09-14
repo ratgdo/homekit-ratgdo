c8a8608266309489af0b212aad29119279bb840d
David Kerr <david@kerrfamily.org>
2025-09-14 12:12:36 -0400

    Support javascript map (.js.map) files for minified javascript by redirecting requests to our GitHub repo.  this avoids 404 Not Found errors when opening console in browser

--- src/web.cpp
+++ src/web.cpp
@@ -128,13 +128,41 @@ char firmwareMD5[36] = "";
 size_t firmwareSize = 0;
 
 // Common HTTP responses
-const char response400missing[] = "400: Bad Request, missing argument\n";
-const char response400invalid[] = "400: Bad Request, invalid argument\n";
-const char response404[] = "404: Not Found\n";
-const char response503[] = "503: Service Unavailable.\n";
-const char response200[] = "HTTP/1.1 200 OK\nContent-Type: text/plain\nConnection: close\n\n";
+constexpr char response400missing[] = "400: Bad Request, missing argument\n";
+constexpr char response400invalid[] = "400: Bad Request, invalid argument\n";
+constexpr char response404[] = "404: Not Found\n";
+constexpr char response503[] = "503: Service Unavailable.\n";
+constexpr char response200[] = "HTTP/1.1 200 OK\nContent-Type: text/plain\nConnection: close\n\n";
 
 const char *http_methods[] = {"HTTP_ANY", "HTTP_GET", "HTTP_HEAD", "HTTP_POST", "HTTP_PUT", "HTTP_PATCH", "HTTP_DELETE", "HTTP_OPTIONS"};
+
+// All this is to support a 303 redirect to js.map files when debugging, so we don't have to embedd in our firmware !!!!
+#define STRINGIFY_HELPER(x) #x
+#define STRINGIFY(x) STRINGIFY_HELPER(x)
+// If not building in main github repo, then add -D GITUSER=your_userid to the compile line (no quotes, STRINGIFY adds that here)
+#ifndef GITUSER
+#define _GITUSER "ratgdo"
+#else
+#define _GITUSER STRINGIFY(GITUSER)
+#endif
+#ifndef GITREPO
+#ifdef ESP8266
+#define _GITREPO "homekit-ratgdo"
+#else
+#define _GITREPO "homekit-ratgdo32"
+#endif
+#else
+#define _GITREPO STRINGIFY(GITREPO)
+#endif
+#ifndef GITBRANCH
+#define _GITBRANCH "main"
+#else
+#define _GITBRANCH STRINGIFY(GITBRANCH)
+#endif
+constexpr char gitUser[] = _GITUSER;
+constexpr char gitRepo[] = _GITREPO;
+constexpr char gitRawURL[] = "https://raw.githubusercontent.com/" _GITUSER "/" _GITREPO "/refs/heads/" _GITBRANCH;
+constexpr char gitTaggedURL[] = "https://raw.githubusercontent.com/" _GITUSER "/" _GITREPO "/refs/tags/v" AUTO_VERSION;
 
 // For Server Sent Events (SSE) support
 // Just reloading page causes register on new channel.  So we need a reasonable number
@@ -539,7 +567,31 @@ void handle_reboot()
 
 void load_page(const char *page)
 {
-    if (webcontent.count(page) == 0)
+    IPAddress clientIP = server.client().remoteIP();
+
+    if ((strlen(page) > 6) && !strcmp(&page[strlen(page) - 6], "js.map"))
+    {
+        // js.map files, also known as JavaScript source maps, are files that provide a mapping between a minified, transpiled,
+        // or bundled JavaScript file and its original, uncompressed source code. The browser only requests this if console/debugger
+        // is opened. We do not store these locally (as large) and will redirect the browser to load from our GitHub repo.
+        if (!strcmp(gitUser, "ratgdo"))
+        {
+            // If we are building on ratgdo (for published release) then use tagged URL to make sure map file matches the one embedded in the firmware
+            strlcpy(writeBuffer, gitTaggedURL, sizeof(writeBuffer));
+        }
+        else
+        {
+            // else we are building for our test purposes, point to the raw URL
+            strlcpy(writeBuffer, gitRawURL, sizeof(writeBuffer));
+        }
+        strlcat(writeBuffer, "/src/www", sizeof(writeBuffer));
+        strlcat(writeBuffer, page, sizeof(writeBuffer));
+        ESP_LOGI(TAG, "Sending 303 redirect to client %s for: %s", clientIP.toString().c_str(), writeBuffer);
+        server.sendHeader(F("Location"), writeBuffer);
+        server.send_P(303, type_txt, "", 0);
+        return;
+    }
+    else if (webcontent.count(page) == 0)
         return handle_notfound();
 
     const unsigned char *data = webcontent.at(page).data;
@@ -571,18 +623,18 @@ void load_page(const char *page)
             server.sendHeader(F("ETag"), crc32);
         if (method == HTTP_HEAD)
         {
-            ESP_LOGI(TAG, "Client %s requesting: %s (HTTP_HEAD, type: %s)", server.client().remoteIP().toString().c_str(), page, type);
+            ESP_LOGI(TAG, "Client %s requesting: %s (HTTP_HEAD, type: %s)", clientIP.toString().c_str(), page, type);
             server.send_P(200, type, "", 0);
         }
         else
         {
-            ESP_LOGI(TAG, "Client %s requesting: %s (HTTP_GET, type: %s, length: %d)", server.client().remoteIP().toString().c_str(), page, type, length);
+            ESP_LOGI(TAG, "Client %s requesting: %s (HTTP_GET, type: %s, length: %d)", clientIP.toString().c_str(), page, type, length);
             server.send_P(200, type, reinterpret_cast<const char *>(data), length);
         }
     }
     else
     {
-        ESP_LOGI(TAG, "Sending 304 not modified to client %s requesting: %s (method: %s, type: %s)", server.client().remoteIP().toString().c_str(), page, http_methods[method], type);
+        ESP_LOGI(TAG, "Sending 304 not modified to client %s requesting: %s (method: %s, type: %s)", clientIP.toString().c_str(), page, http_methods[method], type);
         server.send_P(304, type, "", 0);
     }
     return;
@@ -668,11 +720,7 @@ void handle_status()
 
     // Build the JSON string
     JSON_START(json);
-#ifdef ESP8266
-    JSON_ADD_STR("gitRepo", "homekit-ratgdo");
-#else
-    JSON_ADD_STR("gitRepo", "homekit-ratgdo32");
-#endif
+    JSON_ADD_STR("gitRepo", gitRepo);
     JSON_ADD_INT("upTime", upTime);
     JSON_ADD_STR(cfg_deviceName, userConfig->getDeviceName());
     JSON_ADD_STR("userName", userConfig->getwwwUsername());
