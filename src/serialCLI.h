59b46a0f33baa3a19ac894ca00cdd48ca31e596c
David Kerr <david@kerrfamily.org>
2025-10-05 17:33:02 -0400

    Move serial command line interface (CLI) to separate source file

---
+++ src/serialCLI.h
@@ -0,0 +1,13 @@
+/****************************************************************************
+ * RATGDO HomeKit
+ * https://ratcloud.llc
+ * https://github.com/PaulWieland/ratgdo
+ *
+ * Copyright (c) 2023-25 David A Kerr... https://github.com/dkerr64/
+ * All Rights Reserved.
+ * Licensed under terms of the GPL-3.0 License.
+ *
+ */
+#pragma once
+
+extern void serialCLI(char cmd);
