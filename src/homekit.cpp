ratgdo/homekit-ratgdo32

commit f491c6f4328ad43648ce5d7b845481b57ce48c0e

Make startup initialization sequence same for both ESP8266 and ESP32

src/homekit.cpp
@@ -333,39 +333,20 @@ void connectionCallback(int count)
              WiFi.gatewayIP().toString().c_str(),
              WiFi.dnsIP().toString().c_str());
 
+    if (softAPmode)
+        return;
+
+    // IPv4 Config
+    userConfig->set(cfg_localIP, WiFi.localIP().toString().c_str());
+    userConfig->set(cfg_gatewayIP, WiFi.gatewayIP().toString().c_str());
+    userConfig->set(cfg_subnetMask, WiFi.subnetMask().toString().c_str());
+
+    // Only update cfg_nameserverIP if it is an IPv4 address. .dnsIP() can return an IPv6 address if we have one from SLAAC
+    if (WiFi.dnsIP().type() == IPv4)
+        userConfig->set(cfg_nameserverIP, WiFi.dnsIP().toString().c_str());
+
+    // With WiFi connected, we can now initialize the rest of our app.
     wifi_got_ip = true;
-
-    if (!softAPmode)
-    {
-        // IPv4 Config
-        userConfig->set(cfg_localIP, WiFi.localIP().toString().c_str());
-        userConfig->set(cfg_gatewayIP, WiFi.gatewayIP().toString().c_str());
-        userConfig->set(cfg_subnetMask, WiFi.subnetMask().toString().c_str());
-
-        // Only update cfg_nameserverIP if it is an IPv4 address. .dnsIP() can return an IPv6 address if we have one from SLAAC
-        if (WiFi.dnsIP().type() == IPv4)
-            userConfig->set(cfg_nameserverIP, WiFi.dnsIP().toString().c_str());
-
-        // With WiFi connected, we can now initialize the rest of our app.
-#ifdef RATGDO32_DISCO
-        setup_vehicle();
-#endif
-        setup_comms();
-#ifndef USE_GDOLIB
-        setup_drycontact();
-#endif
-        setup_web();
-    }
-
-#ifdef RATGDO32_DISCO
-    // beep on completing startup.
-    static bool startupBeeped = false;
-    if (!startupBeeped)
-    {
-        tone(BEEPER_PIN, 2000, 500);
-        startupBeeped = true;
-    }
-#endif
 }
 
 void WiFiGotIP(WiFiEvent_t event, WiFiEventInfo_t info)

src/ratgdo.cpp
@@ -185,9 +185,6 @@ void setup()
     // on ESP8266 we setup everything ourselves.
     setup_improv();
     wifi_connect();
-    setup_comms();
-    setup_drycontact();
-    ESP_LOGI(TAG, "Free heap after setup: %d", ESP.getFreeHeap());
 #else
     if (userConfig->getEnableHomeSpanCLI())
         disable_improv();
@@ -196,6 +193,7 @@ void setup()
     // on ESP32 the HomeSpan library has callbacks which we use to setup everything else.
     setup_homekit();
 #endif
+    ESP_LOGI(TAG, "=== WAITING for IP address before continuing initialization");
     // led to idle mode
     led.idle();
 }
@@ -220,10 +218,29 @@ void loop()
 #ifdef ESP8266
         // On ESP8266 we handle WiFi and HomeKit ourselves.  On ESP32 it is done by HomeSpan
         setup_homekit();
+#endif
+#ifdef RATGDO32_DISCO
+        // Initialize vehicle distance sensor
+        setup_vehicle();
+#endif
+        // start communications with garage door opener
+        setup_comms();
+#ifndef USE_GDOLIB
+        setup_drycontact();
+#endif
         // HTTP web server should be started after HomeKit because HomeKit initializes MDNS which we use in web setup
         setup_web();
-#endif
         setup_after_IP_done = true;
+        ESP_LOGI(TAG, "=== Initialization after IP address acquired complete");
+#ifdef RATGDO32_DISCO
+        // beep on completing startup.
+        static bool startupBeeped = false;
+        if (!startupBeeped)
+        {
+            tone(BEEPER_PIN, 2000, 500);
+            startupBeeped = true;
+        }
+#endif
     }
 
     comms_loop();

ratgdo/homekit-ratgdo

commit 381af94087f11c50d34628274d586a2e3a6906fa

Change where we call setup_comms() when we compile with GDOLIB.
For some unknown reason, if we initialize from our main loop() then the comms between s/w serial and GDOLIB task does not work... but if I initialize from the callback in homekit.cpp then it works.  Ideally I should investigate and find the root cause, but for now this workaround does it.  The original change that caused the problem was in commit https://github.com/ratgdo/homekit-ratgdo32/commit/f491c6f4328ad43648ce5d7b845481b57ce48c0e

src/homekit.cpp
@@ -420,6 +420,12 @@ void connectionCallback(int count)
         userConfig->set(cfg_nameserverIP, WiFi.dnsIP().toString().c_str());
 
     // With WiFi connected, we can now initialize the rest of our app.
+#ifdef USE_GDOLIB
+    // start communications with garage door opener
+    // for some unknown reason we need to start GDOLIB comms from this callback and
+    // not in our regular loop.
+    setup_comms();
+#endif
     wifi_got_ip = true;
 }
 

src/ratgdo.cpp
@@ -232,8 +232,14 @@ void setup()
         setup_improv();
 #endif
 
+#ifndef USE_GDOLIB
     // Then initialize communication with the garage door and sensors
+    // When using GDOLIB this is done in homekit.cpp callback. For unknown
+    // reason, if we do it here then SoftwareSerial is not working. I suspect
+    // it is to do with which FreeRTOS task it is started from, because we use
+    // inter-task comms in s/w serial. 
     setup_comms();
+#endif
 #ifdef RATGDO32_DISCO
     setup_vehicle();
 #endif