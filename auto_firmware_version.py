cb3ac6f03d2b70585a85e4d8a900cf2f24fea10b
David Kerr <david@kerr.net>
2025-10-26 20:04:22 -0400

    Added -D VERSION_TAG=txt support to build flags and appened to AUTO_VERSION (from @mitchjs)

--- auto_firmware_version.py
+++ auto_firmware_version.py
@@ -4,15 +4,22 @@ import os.path
 
 Import("env")
 
-def get_firmware_specifier_build_flag():
+def get_firmware_specifier_build_flag(tag = ""):
     f = open('./docs/manifest.json')
     data = json.load(f)
     f.close()
     build_version = data['version'].replace('v', '', 1) #remove letter v from front of version string
-    build_flag = "-D AUTO_VERSION=\\\"" + build_version + "\\\""
-    print ("Firmware Revision: " + build_version)
+    build_flag = "-D AUTO_VERSION=\\\"" + build_version + tag +"\\\""
+    print ("Firmware Version: " + build_version + tag)
     return (build_flag)
 
-# make sure its not already in the BUILD_FLAGS (allows for manual set in platformio.ini)
-if not [bflags for bflags in env.get("BUILD_FLAGS") if "-D AUTO_VERSION=" in bflags]:
-    env.Append(BUILD_FLAGS=[get_firmware_specifier_build_flag()])
+
+# look for "-D VERSION_TAG=" in build flags
+result = next((bflags for bflags in env.get("BUILD_FLAGS") if "-D VERSION_TAG=" in bflags), None)
+verTag = ""
+if result:
+    # create version tag "-xxx"
+    verTag = "-" + result.split("=")[1].strip('"')
+
+# set AUTO_VERSION build flag with optional version tag appended
+env.Append(BUILD_FLAGS=[get_firmware_specifier_build_flag(verTag)])
