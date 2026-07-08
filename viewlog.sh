22196e9d2d103c846bfec4b95b409a3e03524153
Brent Musat <brent.musat@gmail.com>
2026-07-08 14:40:46 -0500

    fix viewlog.sh: log arg silently dropped without a value, cut buffers output when piped
    Bare &log has no value, so the server's parser drops it and the subscription
    never gets log events. Also replaced sed|cut with a single sed command, since
    cut doesn't flush its output until piped into something other than a terminal.

--- viewlog.sh
+++ viewlog.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env sh
 UUID=$(uuidgen)
-URL=$(curl -s "http://${1}/rest/events/subscribe?id=${UUID}&log")
+URL=$(curl -s "http://${1}/rest/events/subscribe?id=${UUID}&log=1&heartbeat=0")
 curl -s "http://${1}/showlog"
-curl -s -N "http://${1}${URL}?id=${UUID}" | sed -u -n '/event: logger/{n;p;}' | cut -c 7-
+curl -s -N "http://${1}${URL}?id=${UUID}" | sed -n -u '/^event: logger$/{n;s/^data: //p;}'
 exit 0
