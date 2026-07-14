ratgdo/homekit-ratgdo

commit ff4c2ad548595043def178aa62a1128074448e0a

add an option to hide the light accessory

src/homekit.cpp
@@ -214,16 +214,39 @@ void setup_homekit()
     config.on_event = homekit_event;
 
     garage_door.has_motion_sensor = (bool)read_door_int(nvram_has_motion);
-    if (!garage_door.has_motion_sensor && (userConfig->getMotionTriggers() == 0))
+    bool show_motion = garage_door.has_motion_sensor || (userConfig->getMotionTriggers() != 0);
+    if (!show_motion)
     {
         ESP_LOGI(TAG, "Motion Sensor not detected.  Disabling Service");
-        config.accessories[0]->services[3] = NULL;
     }
+
+    bool show_light = true;
     if (userConfig->getGDOSecurityType() == 3)
     {
         ESP_LOGI(TAG, "Dry contact does not support light control.  Disabling Service");
-        config.accessories[0]->services[2] = NULL;
+        show_light = false;
     }
+    else if (!userConfig->getLightHomeKit())
+    {
+        ESP_LOGI(TAG, "Light HomeKit accessory disabled in settings.  Disabling Service");
+        show_light = false;
+    }
+
+    // Rebuild the null-terminated service list in-place so that disabling one service does
+    // not truncate the list and accidentally hide any enabled services that follow it.
+    homekit_service_t **services = config.accessories[0]->services;
+    homekit_service_t *light_service = services[2];
+    homekit_service_t *motion_service = services[3];
+    int index = 2; // services[0] (Accessory Information) and services[1] (Garage Door) are always present
+    if (show_light)
+    {
+        services[index++] = light_service;
+    }
+    if (show_motion)
+    {
+        services[index++] = motion_service;
+    }
+    services[index] = NULL;
 
     arduino_homekit_setup(&config);
     homekit_setup_done = true;

src/config.cpp
@@ -476,7 +476,10 @@ userSettings::userSettings()
 #endif
         {cfg_builtInTTC, {false, false, 0, helperBuiltInTTC}},
         {cfg_reverseOnStop, {false, false, true, NULL}},
-#ifndef ESP8266
+#ifdef ESP8266
+        // HomeKit services are static on ESP8266, so a reboot is required to add/remove the light.
+        {cfg_lightHomeKit, {true, false, true, NULL}},
+#else
         // These features not available on ESP8266
         {cfg_occupancyDuration, {false, false, 0, helperOccupancyDuration}}, // call fn to enable/disable HomeKit accessories
         {cfg_enableIPv6, {true, false, false, NULL}},

src/config.h
@@ -90,6 +90,7 @@ constexpr char cfg_useSWserial[] PROGMEM = "useSWserial";
 constexpr char cfg_obstFromStatus[] PROGMEM = "obstFromStatus";
 constexpr char cfg_builtInTTC[] PROGMEM = "builtInTTC";
 constexpr char cfg_reverseOnStop[] PROGMEM = "reverseOnStop";
+constexpr char cfg_lightHomeKit[] PROGMEM = "lightHomeKit";
 #ifdef ESP8266
 // On ESP8266 we save user config to a file in LittleFS
 constexpr char cfg_configFile[] PROGMEM = "user_config";
@@ -108,7 +109,6 @@ constexpr char cfg_TTCsound[] PROGMEM = "TTCsound";
 constexpr char cfg_occupancyDuration[] PROGMEM = "occupancyDuration";
 constexpr char cfg_enableIPv6[] PROGMEM = "enableIPv6";
 constexpr char cfg_homespanCLI[] PROGMEM = "homespanCLI";
-constexpr char cfg_lightHomeKit[] PROGMEM = "lightHomeKit";
 constexpr char cfg_motionHomeKit[] PROGMEM = "motionHomeKit";
 constexpr char cfg_encoderEnabled[] PROGMEM = "encoderEnabled";
 constexpr char cfg_encoderReversed[] PROGMEM = "encoderReversed";
@@ -212,6 +212,7 @@ public:
     bool getObstFromStatus() { return std::get<bool>(get(cfg_obstFromStatus)); };
     uint32_t getBuiltInTTC() { return std::get<int>(get(cfg_builtInTTC)); };
     bool getReverseOnStop() { return std::get<bool>(get(cfg_reverseOnStop)); };
+    bool getLightHomeKit() { return std::get<bool>(get(cfg_lightHomeKit)); };
 #ifdef RATGDO32_DISCO
     uint32_t getVehicleThreshold() { return std::get<int>(get(cfg_vehicleThreshold)); };
     bool getLaserEnabled() { return std::get<bool>(get(cfg_laserEnabled)); };
@@ -230,7 +231,6 @@ public:
     uint32_t getOccupancyDuration() { return std::get<int>(get(cfg_occupancyDuration)); };
     bool getEnableIPv6() { return std::get<bool>(get(cfg_enableIPv6)); };
     bool getEnableHomeSpanCLI() { return std::get<bool>(get(cfg_homespanCLI)); };
-    bool getLightHomeKit() { return std::get<bool>(get(cfg_lightHomeKit)); };
     bool getMotionHomeKit() { return std::get<bool>(get(cfg_motionHomeKit)); };
     bool getEncoderEnabled() { return std::get<bool>(get(cfg_encoderEnabled)); };
     bool getEncoderReversed() { return std::get<bool>(get(cfg_encoderReversed)); };

src/web.cpp
@@ -836,6 +836,7 @@ void build_status_json(char *json)
     JSON_ADD_INT(cfg_logLevel, userConfig->getLogLevel());
     JSON_ADD_INT(cfg_TTCseconds, userConfig->getTTCseconds());
     JSON_ADD_BOOL(cfg_TTClight, userConfig->getTTClight());
+    JSON_ADD_BOOL(cfg_lightHomeKit, userConfig->getLightHomeKit());
     JSON_ADD_INT(cfg_motionTriggers, (uint32_t)motionTriggers.asInt);
     JSON_ADD_INT(cfg_LEDidle, userConfig->getLEDidle());
     // We send milliseconds relative to current time... ie updated X milliseconds ago
@@ -925,7 +926,6 @@ void build_status_json(char *json)
     JSON_ADD_BOOL(cfg_TTCsound, userConfig->getTTCsound());
 #endif
     JSON_ADD_BOOL(cfg_homespanCLI, userConfig->getEnableHomeSpanCLI());
-    JSON_ADD_BOOL(cfg_lightHomeKit, userConfig->getLightHomeKit());
     JSON_ADD_BOOL(cfg_motionHomeKit, userConfig->getMotionHomeKit());
     JSON_ADD_BOOL(cfg_stopDoorHomeKit, userConfig->getStopDoorHomeKit());
 #endif

commit 6f6e9f6a96564a0f2d40d2bc5213fa8b7b570a38

re-add changes from ff4c2ad accidentally removed in e734298

src/homekit.cpp
@@ -214,16 +214,39 @@ void setup_homekit()
     config.on_event = homekit_event;
 
     garage_door.has_motion_sensor = (bool)read_door_int(nvram_has_motion);
-    if (!garage_door.has_motion_sensor && (userConfig->getMotionTriggers() == 0))
+    bool show_motion = garage_door.has_motion_sensor || (userConfig->getMotionTriggers() != 0);
+    if (!show_motion)
     {
         ESP_LOGI(TAG, "Motion Sensor not detected.  Disabling Service");
-        config.accessories[0]->services[3] = NULL;
     }
+
+    bool show_light = true;
     if (userConfig->getGDOSecurityType() == 3)
     {
         ESP_LOGI(TAG, "Dry contact does not support light control.  Disabling Service");
-        config.accessories[0]->services[2] = NULL;
+        show_light = false;
     }
+    else if (!userConfig->getLightHomeKit())
+    {
+        ESP_LOGI(TAG, "Light HomeKit accessory disabled in settings.  Disabling Service");
+        show_light = false;
+    }
+
+    // Rebuild the null-terminated service list in-place so that disabling one service does
+    // not truncate the list and accidentally hide any enabled services that follow it.
+    homekit_service_t **services = config.accessories[0]->services;
+    homekit_service_t *light_service = services[2];
+    homekit_service_t *motion_service = services[3];
+    int index = 2; // services[0] (Accessory Information) and services[1] (Garage Door) are always present
+    if (show_light)
+    {
+        services[index++] = light_service;
+    }
+    if (show_motion)
+    {
+        services[index++] = motion_service;
+    }
+    services[index] = NULL;
 
     arduino_homekit_setup(&config);
     homekit_setup_done = true;